ebc3e99c6660ee46ca1cc6675611dadd7b473b83
ktaehyun <k_taehyun94@naver.com>
2023-02-06 18:04:05 +0900

    main_update

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ print('Searching results:')
 DF = base.df
 for result in results:
    idx = DF[DF['title']==result].index
+   if result[-3:] != '예체능':
+      result = result[:-4]
+   else:
+      result = result[:-5]
+
    if len(idx) == 1:
-      print(f'\t강좌명: {result[:-4]}')
+      print(f'\t강좌명: {result}')
       print(f"\t-> 수강 가능 여부: {DF['condition'][idx[0]]} / 강좌 계열: {DF['kind'][idx[0]]} / URL 강좌 링크: {DF['url'][idx[0]]}\n")
    else:
-      print(f'\t강좌명: {result[:-4]}')
+      print(f'\t강좌명: {result}')
       print(f"\t-> 수강 가능 여부: {DF['condition'][idx[0]]} / 강좌 계열: {DF['kind'][idx[0]]} / URL 강좌 링크: {DF['url'][idx[0]]}\n")
-      print(f'\t강좌명: {result[:-4]}')
+      print(f'\t강좌명: {result}')
       print(f"\t-> 수강 가능 여부: {DF['condition'][idx[1]]} / 강좌 계열: {DF['kind'][idx[1]]} / URL 강좌 링크: {DF['url'][idx[1]]}\n")
