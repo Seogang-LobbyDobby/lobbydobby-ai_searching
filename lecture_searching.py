a87f5d74357b8b4513c59425abfbf17983d0434f
ktaehyun <k_taehyun94@naver.com>
2023-02-14 17:33:15 +0900

    lecture_json

--- lecture_searching.py
+++ lecture_searching.py
@@ -19,10 +19,22 @@ class LectureRs():
             result = result[:-5]
 
          if len(idx) == 1:
-            rs[result] = [f"수강 가능 여부: {DF['condition'][idx[0]]} / 강좌 계열: {DF['kind'][idx[0]]} / URL 강좌 링크: {DF['url'][idx[0]]}"]
+            rs[result] = {
+               0: f"수강 가능 여부: {DF['condition'][idx[0]]}",
+               1: f" 강좌 계열: {DF['kind'][idx[0]]}",
+               2: f"URL 강좌 링크: {DF['url'][idx[0]]}"
+            }
          else:
-            rs[result] = [f"수강 가능 여부: {DF['condition'][idx[0]]} / 강좌 계열: {DF['kind'][idx[0]]} / URL 강좌 링크: {DF['url'][idx[0]]}"]
-            rs[result].append(f"수강 가능 여부: {DF['condition'][idx[1]]} / 강좌 계열: {DF['kind'][idx[1]]} / URL 강좌 링크: {DF['url'][idx[1]]}")
+            rs[result] = {
+               0: f"수강 가능 여부: {DF['condition'][idx[0]]}",
+               1: f"강좌 계열: {DF['kind'][idx[0]]}",
+               2: f"URL 강좌 링크: {DF['url'][idx[0]]}",
+               3: f"수강 가능 여부: {DF['condition'][idx[1]]}",
+               4: f"강좌 계열: {DF['kind'][idx[1]]}",
+               5: f"URL 강좌 링크: {DF['url'][idx[1]]}"
+            }
 
-      rs['editWord'] = input_query
-      return rs
+      RESULT = {'searchResult':[]}
+      RESULT['searchResult'].append(rs)
+      RESULT['editWord'] = input_query
+      return RESULT
