a39cb36b15403b8ed28914a2e18c106489cc2861
ktaehyun <k_taehyun94@naver.com>
2023-02-14 14:03:21 +0900

    fastapi

---
+++ FastApi.py
@@ -0,0 +1,24 @@
+import requests, json, uvicorn
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+from button_searching import Button
+from lecture_searching import LectureRs
+
+
+app = FastAPI()
+lsearching = LectureRs()
+bsearching = Button()
+
+@app.post("/lecture_model/")
+def model_send(username: str = Form()):
+    now_lecture = lsearching.lectureRs(username)
+    return now_lecture
+
+@app.post("/button_model/")
+def model_send(username: str = Form()):
+    now_button = bsearching.button(username)
+    now_button['username'] = now_button.pop('buttonSearching')
+    return now_button
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='192.168.50.55', port=8000)
